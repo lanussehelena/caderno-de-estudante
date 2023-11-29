9d5ae42da29e7831a9c7cb3d3c7c064d08368ca2
lanussehelena <lanusse.dev@gmail.com>
2023-11-29 18:56:30 -0300

    Configurando as rotas, views e controllerda aplicacao

--- app.py
+++ app.py
@@ -4,36 +4,26 @@ from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-# Definindo a variável de ambiente
+# Preparação para o ambiente Flask em desenvolvimento
 os.environ['FLASK_DEBUG'] = 'True'
-
-# Configurando o modo de depuração com base na variável de ambiente
 app.debug = os.environ.get('FLASK_DEBUG') == 'True'
 
+# Rotas do Projeto
 @app.route('/')
-def ola():
+def index():
     return render_template('index.html')
-
-
-@app.route('/sobre')
-def sobre():
-    return render_template('sobre.html')
-
-
+ 
 @app.route('/glossario')
 def glossario():
 
-    glossario_de_termos = []
+    glossario_de_termo = []
 
-    with open(
-            'bd_glossario.csv',
-            newline='', encoding='utf-8') as arquivo:
+    with open('bd_glossario.csv', newline='', encoding='utf-8') as arquivo:
         reader = csv.reader(arquivo, delimiter=';')
-        for l in reader:
-            glossario_de_termos.append(l)
+        for i in reader:
+            glossario_de_termo.append(i)
 
-    return render_template('glossario.html',
-                           glossario=glossario_de_termos)
+    return render_template('glossario.html', glossario_de_termos=glossario_de_termo)
 
 
 @app.route('/novo_termo')
@@ -46,9 +36,7 @@ def criar_termo():
     termo = request.form['termo']
     definicao = request.form['definicao']
 
-    with open(
-            'bd_glossario.csv', 'a',
-            newline='', encoding='utf-8') as arquivo:
+    with open('bd_glossario.csv', 'a', newline='', encoding='utf-8') as arquivo:
         writer = csv.writer(arquivo, delimiter=';')
         writer.writerow([termo, definicao])
 
@@ -58,36 +46,71 @@ def criar_termo():
 @app.route('/excluir_termo/<int:termo_id>', methods=['POST'])
 def excluir_termo(termo_id):
 
-    with open('bd_glossario.csv', 'r', newline='') as file:
+    with open('bd_glossario.csv', 'r',  newline='') as file:
         reader = csv.reader(file)
         linhas = list(reader)
 
-    # Encontrar e excluir o termo com base no ID
     for i, linha in enumerate(linhas):
         if i == termo_id:
             del linhas[i]
             break
 
-    # Salvar as alterações de volta no arquivo
     with open('bd_glossario.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(linhas)
 
     return redirect(url_for('glossario'))
 
-# @app.route('/pesquisar_termo/<int:termo_id>')
-# def pesquisar_termo(termo_id):
-#
-#     with open('bd_glossario.csv', 'r', newline='') as file:
-#         reader = csv.reader(file)
-#         linhas = list(reader)
-#
-#     # Encontrar e excluir o termo com base no ID
-#     for i, linha in enumerate(linhas):
-#         if i == termo_id:
-#             del linhas[i]
-#             break
 
+@app.route('/nova_tarefa')
+def nova_tarefa():
+    return render_template('adicionar_tarefa.html')
+
+@app.route('/tarefas')
+def tarefa():
+    lista_de_tarefas = []
+    with open('bd_tarefas.csv', newline='', encoding='utf-8') as arquivo:
+        reader = csv.reader(arquivo, delimiter=';')
+        for i in reader:
+            lista_de_tarefas.append(i)
+
+    return render_template('tarefas.html', tarefas=lista_de_tarefas)
+
+
+@app.route('/cria_tarefa', methods=['POST', ])
+def cria_tarefa():
+    tarefas = request.form['tarefa']
+    descricao = request.form['descricao']
+
+    with open('bd_tarefas.csv', 'a', newline='', encoding='utf-8') as arquivo:
+        writer = csv.writer(arquivo, delimiter=';')
+        writer.writerow([tarefas, descricao])
+
+    return redirect(url_for('tarefa'))
+
+
+@app.route('/excluir_tarefa/<int:tarefa_id>', methods=['POST'])
+def excluir_tarefa(tarefa_id):
+
+    with open('bd_tarefas.csv', 'r',  newline='') as file:
+        reader = csv.reader(file)
+        linhas1 = list(reader)
+
+    for i, linha in enumerate(linhas1):
+        if i == tarefa_id:
+            del linhas1[i]
+            break
+
+    with open('bd_tarefas.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(linhas1)
+
+    return redirect(url_for('tarefa'))
+
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
 
 
 if __name__ == "__main__":
